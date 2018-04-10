66a1cf58339fc31c6e218d0ba43fdcb96e85799f
Romain Billard <romain.billard@hetic.net>
2018-04-10 10:20:38 +0200

    Add basic oclif setup

---
+++ src/commands/hello.js
@@ -0,0 +1,22 @@
+// @flow
+import { Command, flags } from '@oclif/command'
+
+class HelloCommand extends Command {
+  async run() {
+    const { flags } = this.parse(HelloCommand)
+    const name = flags.name || 'world'
+    this.log(`hello ${name} from ./src/commands/hello.js`)
+  }
+}
+
+HelloCommand.description = `
+Describe the command here
+...
+Extra documentation goes here
+`
+
+HelloCommand.flags = {
+  name: flags.string({ char: 'n', description: 'name to print' })
+}
+
+module.exports = HelloCommand
