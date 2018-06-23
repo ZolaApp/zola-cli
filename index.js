72623a0dde4455b76cbd616a2666bb1810a6cd55
Tom Bonnike <bonniketom@gmail.com>
2018-06-23 18:45:05 +0200

    Bump version

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const handleDefault = () => {
 // We need this because fetch has an issue with our SSL certs atm
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
-program.version('0.3.0')
+program.version('0.3.2')
 
 program
   .command('login <email>')
