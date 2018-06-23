0dcd53dc95f91215f94abe21fd2277c0ee2993ed
Tom Bonnike <bonniketom@gmail.com>
2018-06-23 17:19:24 +0200

    Add base of extract command

--- commands/extract.js
+++ commands/extract.js
@@ -1,24 +1,65 @@
+const { flatten, uniq } = require('lodash')
+const walk = require('klaw')
+const babel = require('babel-core')
 const ora = require('ora')
-const getRandom = (min, max) => Math.floor(Math.random() * max) + min
-const wait = timeout => new Promise(resolve => setTimeout(resolve, timeout))
 
-const handleExtract = () => {
-  const keysCount = getRandom(50, 120)
+const getAllFiles = async directory =>
+  new Promise((resolve, reject) => {
+    const items = []
+
+    walk(process.env.INIT_CWD)
+      .on('data', item => {
+        const { path } = item
+
+        if (path.endsWith('.js') && !path.includes('node_modules')) {
+          items.push(path)
+        }
+      })
+      .on('end', () => resolve(items))
+  })
+
+const parseFile = async file =>
+  new Promise((resolve, reject) => {
+    babel.transformFile(
+      file,
+      { presets: ['env', 'react'], plugins: ['react-intl'] },
+      (err, result) => {
+        if (err) {
+          // Swallow error.
+          return resolve([])
+        }
+
+        return resolve(result.metadata['react-intl'].messages)
+      }
+    )
+  })
+
+const extractKeys = async files => {
+  const keys = await Promise.all(files.map(parseFile))
+
+  return uniq(flatten(keys))
+}
+
+const handleExtract = async () => {
   const parsing = ora('Parsing project…')
-  const uploading = ora('Uploading…')
-
   parsing.start()
 
-  wait(3000).then(() => {
-    parsing.text = `${keysCount} new keys found…`
-    parsing.succeed()
-    uploading.start()
+  const files = await getAllFiles()
+  parsing.succeed()
 
-    wait(1000).then(() => {
-      uploading.text = `🚀  ${keysCount} keys uploaded.`
-      uploading.succeed()
-    })
-  })
+  const extraction = ora('Extracting keys…')
+  extraction.start()
+
+  const keys = await extractKeys(files)
+  extraction.text = `🔎  ${keys.length} keys found.`
+  extraction.succeed()
+
+  console.log(JSON.stringify(keys))
+  // const uploading = ora('Uploading keys…')
+  // uploading.start()
+
+  // uploading.text = `🚀  ${keys.length} keys uploaded.`
+  // uploading.succeed()
 }
 
 module.exports = {
